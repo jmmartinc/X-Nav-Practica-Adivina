9e56ac6b37462a0f53d2f2a62843e3eedf4c2042
Jose María Martín Celada <josemc90@gmail.com>
2015-05-13 20:26:15 +0200

    Funcionalidad básica terminada

--- practica.js
+++ practica.js
@@ -1,15 +1,40 @@
-$(document).ready(function(){
-    var map = ""
+$(document).ready(function() {
+    var map = "";
+    var marker = "";
+    var correctMarker = "";
     var nimages = 20;
     var timeoutImages = "";
     var time = 10000;
+    var imgShown = 0;
+    var coordinates = "";
+    var maxDistance = 15409;
+    var items = [];
+    var city = "";
+    var latitude = 0;
+    var longitude = 0;
 
+    
+    getData();
     newGame();
 
+    function getData() {
+        $.ajax({ 
+            url: "capitales.json", 
+            dataType: 'json',  
+            async: false, 
+            success: function(data){ 
+                city = data.city[0].name;
+                latitude = data.city[0].lat;
+                longitude = data.city[0].lon;
+            } 
+        });
+    }
+
     function newGame() {
+        $("#next").hide();
         resetMap();
         resetImages();
-        getPhotos("madrid");
+        getPhotos(city);
     }
 
     function resetMap() {
@@ -20,26 +45,52 @@ $(document).ready(function(){
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
+        marker = L.marker([0,0],{opacity:0});
+        marker.addTo(map);
+        map.on('click', function(pos) {
+            marker.setLatLng(pos.latlng); 
+            marker.setOpacity(1);
+        });
+        coordinates = L.latLng(latitude,longitude);
+        correctMarker = L.marker([latitude,longitude],{opacity:0});
+        correctMarker.addTo(map);
+    }
+
+    function drawLine() {
+        var line = [];
+        line.push(marker.getLatLng());
+        line.push(correctMarker.getLatLng());
+        var polyline = L.polyline(line, {color: '#222'}).addTo(map);
     }
 
     function getPhotos(tag) {
         $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?tags=' +
         tag + '&tagmode=any&format=json&jsoncallback=?', function(data){
             var item = data.items.splice(0,nimages);
-            var i = 0;
-            $("#image").attr('src',item[i].media.m);
+            $("#image").attr('src',item[imgShown].media.m);
             timeoutImages = setInterval(function(){     // Cambia
-                i++;
-                $("#image").attr('src',item[i % nimages].media.m);
+                imgShown++;
+                $("#image").attr('src',item[imgShown % nimages].media.m);
             }, time);
         });
     }
 
-    function resetImages(){
+    function resetImages() {
         if (timeoutImages != "") {
             clearTimeout(timeoutImages);
             timeoutImages = "";
+            imgShown = 0;
         }
+    }
+
+    function getDistance() {
+        return coordinates.distanceTo(marker.getLatLng())/1000;
+    }
+
+    function getScore() {
+        if (imgShown >= 20)
+            imgShown = 19;
+        return Math.floor( ( (maxDistance*nimages) - (imgShown+1)*getDistance() ) * 0.0032 );
     }
 
     $("#easy").click(function(){
@@ -58,6 +109,16 @@ $(document).ready(function(){
     });
 
     $("#accept").click(function(){
-        resetImages();
+        if (marker.getLatLng().lat != 0 || marker.getLatLng().lng != 0) {
+            correctMarker.setOpacity(1);
+            drawLine();
+            $("#score").html(getScore());
+            resetImages();
+            $("#next").show();
+        }
+    });
+
+    $("#next").click(function(){
+        newGame();
     });
 });
