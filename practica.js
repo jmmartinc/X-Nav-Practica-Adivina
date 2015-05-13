679e60bbd97b8262f14ab2e29902aa4b1cf3b2d9
Jose María Martín Celada <josemc90@gmail.com>
2015-05-13 14:58:43 +0200

    Interfaz mejorada

--- practica.js
+++ practica.js
@@ -1,8 +1,63 @@
 $(document).ready(function(){
-    var map = L.map('map').setView([45, 11], 2);
+    var map = ""
+    var nimages = 20;
+    var timeoutImages = "";
+    var time = 10000;
 
-    // add an OpenStreetMap tile layer
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    newGame();
+
+    function newGame() {
+        resetMap();
+        resetImages();
+        getPhotos("madrid");
+    }
+
+    function resetMap() {
+        if (map != "") {
+            map.remove();
+        }
+        map = L.map('map').setView([45, 11], 2);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    }
+
+    function getPhotos(tag) {
+        $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?tags=' +
+        tag + '&tagmode=any&format=json&jsoncallback=?', function(data){
+            var item = data.items.splice(0,nimages);
+            var i = 0;
+            $("#image").attr('src',item[i].media.m);
+            timeoutImages = setInterval(function(){     // Cambia
+                i++;
+                $("#image").attr('src',item[i % nimages].media.m);
+            }, time);
+        });
+    }
+
+    function resetImages(){
+        if (timeoutImages != "") {
+            clearTimeout(timeoutImages);
+            timeoutImages = "";
+        }
+    }
+
+    $("#easy").click(function(){
+        time = 10000;
+        newGame();
+    });
+
+    $("#medium").click(function(){
+        time = 5000;
+        newGame();
+    });
+
+    $("#hard").click(function(){
+        time = 1000;
+        newGame();
+    });
+
+    $("#accept").click(function(){
+        resetImages();
+    });
 });
